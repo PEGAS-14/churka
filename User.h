361c5ce60170992c812ac529ae1967e091be7769
PEGAS-14 <103308802+PEGAS-14@users.noreply.github.com>
2022-04-09 12:29:14 +0300

    Add files via upload

---
+++ User.h
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class User {
+private :
+	string name;
+	string adress;
+	double balance;
+	double spent;
+public:
+	User(string name, string adress, double balance, double spent) {
+		this->name = name;
+		this->adress = adress;
+		this->balance = balance;
+		this->spent = spent;
+	}
+	void ReduceBalance(double price) {
+		balance -= price;
+		spent += price;
+	}
+	void Print() {
+		cout << name << "\t" << adress << balance << "\t" << spent <<
+			endl;
+	}
+};
